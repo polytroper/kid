c09f553d20727a3e1afb31593fc6824d558e4d6a
Christopher Walker <cwalker@polytrope.com>
2019-03-20 17:26:27 -0400

    Initial commit
    FIRST POST!!!!!!!!!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kid
+
+*A small child. They look rather pathetic. Poor kid*
+
+scuse me, scuse me sirrah i lost my cat... please help me find my kitty *cough cough* :(
---
+++ index.js
@@ -0,0 +1,117 @@
+var Botkit = require('botkit')
+var Airtable = require('airtable')
+var _ = require('lodash')
+
+var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base(process.env.AIRTABLE_BASE);
+
+var redisConfig = {
+  url: process.env.REDISCLOUD_URL
+}
+var redisStorage = require('botkit-storage-redis')(redisConfig)
+
+console.log("Booting kid bot")
+
+var startState = 'Cat Requested'
+
+function createCatQuestState(user, cb = () => {}) {
+  console.log(`Creating balance for User ${user}`)
+  
+  base('States').create({
+    "User": user,
+    "CatQuest": startState
+  }, function(err, record) {
+      if (err) { console.error(err); return; }
+      console.log(`New record created for User ${user}`)
+      // console.log(record)
+      cb(startState, record)
+  });
+}
+
+function setCatQuestState(id, state, cb = () => {}) {
+  console.log(`Setting CatQuest for Record ${id} to ${state}`)
+
+  base('States').update(id, {
+    "CatQuest": state
+  }, function(err, record) {
+    if (err) { console.error(err); return; }
+    console.log(`CatQuest for Record ${id} set to ${state}`)
+    cb(state, record)
+  })
+}
+
+function getCatQuestState(user, cb = () => {}) {
+  console.log(`Retrieving CatQuest for User ${user}`)
+
+  base('States').select({
+    filterByFormula: `User = "${user}"`
+  }).firstPage(function page(err, records) {
+    if (err) {
+      console.error(err)
+      return
+    }
+
+    if (records.length == 0) {
+      console.log(`No CatQuest state found for User ${user}.`)
+      createCatQuestState(user, cb)
+    }
+    else {
+      var record = records[0]
+      var fields = record.fields
+      var state = fields['CatQuest']
+      console.log(`CatQuest state for User ${user} is ${state}`)
+      console.log(fields)
+      cb(balance, record)
+    }
+  })
+}
+
+var controller = Botkit.slackbot({
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  clientSigningSecret: process.env.SLACK_CLIENT_SIGNING_SECRET,
+  scopes: ['bot', 'chat:write:bot'],
+  storage: redisStorage
+});
+
+controller.setupWebserver(process.env.PORT, function(err,webserver) {
+    controller.createWebhookEndpoints(controller.webserver)
+    controller.createOauthEndpoints(controller.webserver)
+});
+
+var bot = controller.spawn({
+});
+
+bot.say({
+  text: 'Awake',
+  channel: '@UDK5M9Y13'
+});
+
+// @bot hello --> Begins the Cat Rescue quest
+controller.hears(/hello/i, 'direct_message', (bot, message) => {
+  // console.log(message)
+  var {text, user} = message
+
+  console.log(`Processing hello from ${user}`)
+
+  var keys = ['target', 'amount']
+
+  var catNames = [
+    'floofy',
+    'Mr. Poops',
+    'furryface',
+    'pillowpants',
+    'ploofyploof'
+  ]
+
+  var catName = _.sample(catNames)
+  var catNameMistake = _.sample(catNameMistake)
+
+  bot.reply(message, `scuse me sirrah, scuse me...\nmy cat ${catName} is missing.\n\npoor ${catName} _cough cough_ :(`)
+})
+
+controller.hears('.*', 'direct_mention,direct_message', (bot, message) => {
+  var {text, user} = message
+  console.log(`Received unhandled message from User ${user}:\n${text}`)
+
+  bot.replyInThread(message, 'oh uhmm sorry I don\'t understand...')
+})
